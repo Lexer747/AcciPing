4ab261859f600363c8a24f0e11b4ec093628c688
Lexer747 <Lexer747@gmail.com>
2024-08-19 18:51:45 +0100

    WIP

--- graph/graphdata/graphdata.go
+++ graph/graphdata/graphdata.go
@@ -8,7 +8,9 @@ package graphdata
 
 import (
 	"io"
+	"math"
 	"sync"
+	"time"
 
 	"github.com/Lexer747/AcciPing/graph/data"
 	"github.com/Lexer747/AcciPing/ping"
@@ -32,18 +34,29 @@ func NewGraphData(d *data.Data) *GraphData {
 		m: &sync.Mutex{},
 	}
 	for i := range d.TotalCount {
-		new := g.spans[g.spanIndex].AddPoint(d.Get(i))
-		if new {
-			panic("todo")
-		}
+		g.addPointToSpans(d.Get(i))
 	}
 	return g
+}
+
+func (gd *GraphData) addPointToSpans(p ping.PingDataPoint) {
+	differentSpan := gd.spans[gd.spanIndex].AddPoint(p)
+	if differentSpan {
+		gd.spans = append(gd.spans, &SpanInfo{
+			TimeStats: &data.Stats{},
+			TimeSpan:  &data.TimeSpan{},
+			LastPoint: ping.PingDataPoint{},
+		})
+		gd.spanIndex++
+		gd.spans[gd.spanIndex].AddPoint(p)
+	}
 }
 
 func (gd *GraphData) AddPoint(p ping.PingResults) {
 	gd.Lock()
 	defer gd.Unlock()
 	gd.data.AddPoint(p)
+	gd.addPointToSpans(p.Data)
 }
 
 func (gd *GraphData) TotalCount() int64 {
@@ -106,8 +119,42 @@ func (si *SpanInfo) AddPoint(p ping.PingDataPoint) bool {
 		add()
 		return false
 	}
-
+	// Problem statement:
+	//
+	// We want to determine if a given new point is essentially from a new sampling domain. The main use case
+	// is that we capture some packets on day 1. Then capture some more packets on day 2. When something like
+	// this happens we want to essentially plot two related graphs on a single axis.
+	//
+	// In general anytime we encounter some large gap between samples we want to split the graph by that gap
+	// to maximizes the information we are displaying (especially given the small amount of space we are
+	// typically working with). However one obvious consequence which should be accounted for is that dropped
+	// packets already live in their own sampling domain. As the timeout will not be the same as the ping
+	// ticker frequency. Furthermore it's a requirement that two different frequency samples can be appended
+	// together should be treated equally, e.g. a capture running at 5 pings/minute on day 1 would still look
+	// good on the same graph when day 2 is 100 pings/minute.
+	//
+	// Solution:
+	//
+	// We record the difference in timestamps into a [data.Stats] struct which will work out the statistical
+	// nature of the current sampling, if we detect the next point is some outlier then we consider a new
+	// span. Where outlier is a flexible definition to just mean whatever is the best heuristic for pretty
+	// graphs.
 	gap := p.Timestamp.Sub(si.LastPoint.Timestamp)
+	allowedStandardDeviations := 3.0
+	if p.Dropped() {
+		// At low ping rate this might be too high, given a reasonable 1 ping/minute, a 1s timeout is
+		// completely reasonable in which case this should just stay as 3 stds away. Scale this somehow?
+		allowedStandardDeviations = 6.0
+	}
+	if time.Duration(math.Round(si.TimeStats.StandardDeviation*allowedStandardDeviations)) < gap {
+		// This gap is officially too big, don't add this point.
+		// TODO account for very early small stats with low confidence
+		return true
+	}
+	// This gap is small enough add it to this span
+	si.TimeStats.AddPoint(gap)
+	add()
+	return false
 }
 
 type Iter struct {
